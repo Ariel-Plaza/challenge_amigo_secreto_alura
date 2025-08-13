ce1800d125552083fcd3a022cc77237da96f63f6
Ariel Plaza <arielplazasalinas@gmail.com>
2025-08-12 22:26:29 -0300

    se agrega ciclo for para agregar nombres de amigos a listaAmigos

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = []
-let amigo = document.getElementById("amigo").value;
+// let amigo = document.getElementById("amigo").value;
 let listaAmigos = document.getElementById("listaAmigos")
 
 function agregarAmigo() {
+  let amigo = document.getElementById("amigo").value;  
+  console.log(amigo)
   if (amigo != "") {
     amigos.push(amigo);
-    console.log(amigos);
   } else {
-    alert("Por favor, inserte un nombre.")
+    alert("Por favor, inserte un nombre.");
   }
+  lista = ""
+  for (let i = 0; i < amigos.length; i++) {
+    lista += `<li>${amigos[i]}`
+  }
+  listaAmigos.innerHTML = lista;
 }
