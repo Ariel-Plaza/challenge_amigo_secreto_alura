c7451a38963a68bd7f35d25ac0ad031ed29ba040
Ariel Plaza <arielplazasalinas@gmail.com>
2025-08-12 21:38:23 -0300

    Creacion de funcion agregarAmigo(), agrega amigos en array

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = []
+let amigo = document.getElementById("amigo").value;
+let listaAmigos = document.getElementById("listaAmigos")
+
+function agregarAmigo() {
+  if (amigo != "") {
+    amigos.push(amigo);
+    console.log(amigos);
+  } else {
+    alert("Por favor, inserte un nombre.")
+  }
+}
